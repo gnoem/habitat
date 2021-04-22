e3da5987b4befcec6adae745c55987f5a19dab2c
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-04-21 20:11:23 -0700

    made calendar days clickable

--- components/Calendar/index.js
+++ components/Calendar/index.js
@@ -1,8 +1,10 @@
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dayjs from "dayjs";
 import { fancyClassName } from "../../utils";
 import styles from "./calendar.module.css";
 
-export const Calendar = ({ habits, entries, calendarPeriod }) => {
+export const Calendar = ({ habits, entries, calendarPeriod, updateDashPanel }) => {
   const daysInMonth = new Array(dayjs(calendarPeriod).daysInMonth()).fill('').map((_, index) => {
     return dayjs(`${calendarPeriod}-${index + 1}`).format('YYYY-MM-DD');
   });
@@ -21,7 +23,7 @@ export const Calendar = ({ habits, entries, calendarPeriod }) => {
   return (
     <div className={styles.Calendar}>
       <CalendarWeekLabels />
-      <CalendarDays {...{ habits, entries, totalDaysInMonth }} />
+      <CalendarDays {...{ habits, entries, totalDaysInMonth, updateDashPanel }} />
     </div>
   );
 }
@@ -37,7 +39,7 @@ const CalendarWeekLabels = () => {
   });
 }
 
-const CalendarDays = ({ habits, entries, totalDaysInMonth }) => {
+const CalendarDays = ({ habits, entries, totalDaysInMonth, updateDashPanel }) => {
   return totalDaysInMonth.map((date, index) => {
     const isFiller = date === '';
     const { records } = entries.find(entry => entry.date === date) ?? {};
@@ -48,22 +50,25 @@ const CalendarDays = ({ habits, entries, totalDaysInMonth }) => {
       );
     });
     return (
-      <CalendarDay key={`calendarDay-${index}`} className={isFiller ? styles.filler : ''}>
-        {isFiller
-          ? null
-          : <>
-              <div>{date.split('-')[2]}</div>
-              <div>{recordIcons}</div>
-            </>
-        }
+      <CalendarDay key={`calendarDay-${index}`}
+                   className={isFiller ? styles.filler : ''}
+                   date={date}
+                   updateDashPanel={updateDashPanel}>
+        {isFiller ? null : (
+          <div className={styles.title}>
+            {date.split('-')[2]}
+          </div>
+        )}
+        <div>{recordIcons}</div>
       </CalendarDay>
     );
   });
 }
 
-const CalendarDay = ({ children, className }) => {
+const CalendarDay = ({ children, className, date, updateDashPanel }) => {
   return (
-    <div className={`${styles.calendarDay} ${fancyClassName({ styles, className })}`}>
+    <div className={`${styles.calendarDay} ${fancyClassName({ styles, className })}`}
+         onClick={() => updateDashPanel('data', { date })}>
       {children}
     </div>
   );
