541800d76dc7ea59e91a4cdf38d284e42e728b94
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-04-22 23:41:33 -0700

    added progress spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/global.css";
-import { Layout } from "../components/Layout";
+import Router from "next/router";
+import nprogress from "nprogress";
 import { AppContextProvider } from "../contexts";
+import { Layout } from "../components/Layout";
+
+Router.events.on('routeChangeStart', () => nprogress.start());
+Router.events.on('routeChangeComplete', () => nprogress.done());
+Router.events.on('routeChangeError', () => nprogress.done());
 
 const App = ({ Component, pageProps }) => {
   return (
