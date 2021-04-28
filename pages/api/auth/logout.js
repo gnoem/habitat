1d33bbe0c79e7979e7900113df3fdaa9f078aab1
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-04-27 20:09:30 -0700

    fixed logout issue with unexpected end of json input

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -2,5 +2,5 @@ import withSession from "../../../lib/session";
 
 export default withSession(async (req, res) => {
   req.session.destroy();
-  res.status(204).end()
+  res.status(200).send({})
 });
