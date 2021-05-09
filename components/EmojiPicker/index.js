970e2d404db2104092be868030007232962926c0
Gnome <68836321+gnomelabs@users.noreply.github.com>
2021-05-08 23:31:33 -0700

    fixed issue where emoji picker button onclick would close modal container
    because doing so would unrender the component and then e technically modalContent does not contain e.target

--- components/EmojiPicker/index.js
+++ components/EmojiPicker/index.js
@@ -1,21 +1,30 @@
 import dynamic from "next/dynamic";
 import { useEffect, useRef, useState } from "react";
+
+import styles from "./emojiPicker.module.css";
+
 const EmojiPickerNoSSRWrapper = dynamic(
   () => import("emoji-picker-react"),
   {
     ssr: false,
-    loading: () => <p>Loading...</p>,
+    loading: () => <aside className={styles.loading}>Loading...</aside>,
   }
 );
-
-import styles from "./emojiPicker.module.css";
 
 const EmojiPicker = ({ setFormData }) => {
   const [expanded, setExpanded] = useState(false);
   const containerRef = useRef(null);
-  const toggleExpanded = () => setExpanded(state => !state);
+  const toggleExpanded = () => {
+    if (expanded) setExpanded('closing');
+    else setExpanded(true);
+  }
   useEffect(() => {
     if (!expanded) return;
+    if (expanded === 'closing') {
+      setTimeout(() => {
+        setExpanded(false)
+      }, 200);
+    }
     // todo - if in modal, set height of emoji picker element dynamically based on distance to bottom of container
     // or maybe figure out its own scroll height and then subtract 16px and set it to that while keeping the modal box height fixed
     // that seems insane though
@@ -23,23 +32,23 @@ const EmojiPicker = ({ setFormData }) => {
     const closePicker = (e) => {
       if (!containerRef.current?.contains(e.target)) {
         console.log('thats whats up');
-        setExpanded(false);
+        setExpanded('closing');
       }
     }
     window.addEventListener('click', closePicker);
     return () => window.removeEventListener('click', closePicker);
   }, [expanded]);
-  const handleClick = (e, obj) => {
-    setExpanded(false);
+  const handleClick = (_, obj) => {
+    setExpanded('closing');
     setFormData(prevData => ({
       ...prevData,
       icon: obj.emoji
     }));
   }
   return (
-    <div className={styles.EmojiPicker} ref={containerRef}>
+    <div className={`${styles.EmojiPicker} ${(expanded === 'closing') ? styles.goodbye : ''}`} ref={containerRef}>
       <button type="button" onClick={toggleExpanded}>{expanded ? 'close' : 'emoji picker'}</button>
-      {expanded && <EmojiPickerNoSSRWrapper onEmojiClick={handleClick} />}
+      {expanded && <EmojiPickerNoSSRWrapper onEmojiClick={handleClick} disableAutoFocus />}
     </div>
   );
 }
